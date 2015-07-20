354ceed72d24216ddc3948e061ed8c06f97d8e96
Peter Schlaile <peter@schlaile.de>
2015-07-20 19:23:15 +0200

    German locale

---
+++ src/internationalization/locales/PhutilGermanLocale.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The german locale.
+ */
+final class PhutilUSEnglishLocale extends PhutilLocale {
+
+  public function getLocaleCode() {
+    return 'de_DE';
+  }
+
+  public function getLocaleName() {
+    return pht('German (DE)');
+  }
+
+}
